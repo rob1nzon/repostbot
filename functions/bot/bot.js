f92153db9c94d0d730105e12cbc26f81be4a5ea3
Denis Chencov <denis@rob1nzon.ru>
2023-12-08 15:23:41 +0300

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require('telegraf')
+const { newPost } = require('../components/fauna')
 const startAction = require('./actions/start')
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
@@ -22,8 +23,20 @@ bot.on('message', (ctx) => {
   try {
     console.log(ctx.message?.forward_from_chat.id)
     console.log(ctx.message?.forward_from_message_id)
-    ctx.reply('ok')
+    let postId = ctx.message?.forward_from_chat.id + ctx.message?.forward_from_message_id;
+    let isPostUser = await newPost(id)
+    if (isPostUser) {
+      return ctx.reply('Added post to db!')
+    }else{
+      return ctx.reply('Post is already inside db!')
+    }
+   
+    } catch (e) {
+      return ctx.reply(`Error occured`)
+    }
   } catch (error) {
     console.error(error)
   }
 })
+
+
